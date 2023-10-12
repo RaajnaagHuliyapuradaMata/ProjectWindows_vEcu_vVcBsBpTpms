e7f3fe4bef8a6e7224ac91ec3b5493811c17c885
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-12 13:22:28 +0200

    ATUOSAR - Hotfixes - pIntegrationRdcXnf

---
+++ cB_LATEST/b_git_push.sh
@@ -0,0 +1,20 @@
+cd ./source/common
+./b_git_push.sh
+
+cd ../ProjectARA
+./b_git_push.sh
+
+cd ../ProjectCfg
+./b_git_push.sh
+
+cd ./ProjectCompiler
+./b_git_push.sh
+
+cd ./SwcApplTpms
+./b_git_push.sh
+
+cd ..
+git add .
+git status
+git commit -m "AUTOSAR - Hotfixes - cB_LATEST"
+git push
