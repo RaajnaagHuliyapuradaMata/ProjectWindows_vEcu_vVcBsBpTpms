cd3e182d4a5413e4b4d9f5f623c1ee268951bbf1
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-10-19 10:52:54 +0200

    AUTOSAR - Hotfixes - cB_LATEST

--- cB_LATEST/b_git_push.sh
+++ cB_LATEST/b_git_push.sh
@@ -1,16 +1,16 @@
 cd ./source/common
 ./b_git_push.sh
 
-cd ../ProjectARA
+cd ../Ara
 ./b_git_push.sh
 
 cd ../ProjectCfg
 ./b_git_push.sh
 
-cd ./ProjectCompiler
+cd ../ProjectCompiler
 ./b_git_push.sh
 
-cd ./SwcApplTpms
+cd ../SwcApplTpms
 ./b_git_push.sh
 
 cd ../..
