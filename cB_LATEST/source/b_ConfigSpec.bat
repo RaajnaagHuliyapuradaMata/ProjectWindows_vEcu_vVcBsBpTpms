c8aed1c0384a66be71011dbc9f9e476ade6a1634
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-11-02 15:28:50 +0100

    AUTOSAR Migration Activities

---
+++ cB_LATEST/source/b_ConfigSpec.bat
@@ -0,0 +1,35 @@
+DEL Ara
+MKLINK /D Ara             ..\..\..\..\repo_submodules\ProjectARA\pIntegrationRdcXnf_cB_LATEST
+
+DEL common
+MKLINK /D common          ..\..\..\..\repo_submodules\common\pIntegrationRdcXnf_cB_LATEST
+
+DEL ProjectCfg
+MKLINK /D ProjectCfg      ..\..\..\..\repo_submodules\ProjectCfg\pIntegrationRdcXnf_cB_LATEST
+
+DEL ProjectCompiler
+MKLINK /D ProjectCompiler ..\..\..\..\repo_submodules\ProjectCompiler\pIntegrationRdcXnf_cB_LATEST
+
+DEL ReSimVehicle
+MKLINK /D ReSimVehicle    ..\..\..\..\repo_submodules\ReSimVehicle\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcApplDcm
+MKLINK /D SwcApplDcm      ..\..\..\..\repo_submodules\SwcApplDcm\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcApplDem
+MKLINK /D SwcApplDem      ..\..\..\..\repo_submodules\SwcApplDem\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcApplNvM
+MKLINK /D SwcApplNvM      ..\..\..\..\repo_submodules\SwcApplNvM\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcApplTpms
+MKLINK /D SwcApplTpms     ..\..\..\..\repo_submodules\SwcApplTpms\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcServiceDcm
+MKLINK /D SwcServiceDcm   ..\..\..\..\repo_submodules\SwcServiceDcm\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcServiceDem
+MKLINK /D SwcServiceDem   ..\..\..\..\repo_submodules\SwcServiceDem\pIntegrationRdcXnf_cB_LATEST
+
+DEL SwcServiceNvM
+MKLINK /D SwcServiceNvM   ..\..\..\..\repo_submodules\SwcServiceNvM\pIntegrationRdcXnf_cB_LATEST
